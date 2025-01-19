547c334418594b4be5910151801ff1c3177f2d8b
balfajr <balfajr@>
2025-01-19 14:58:04 +0700

    Update website

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -5,7 +5,7 @@ function Footer({github}) {
   return (
     <div className="text-center w-full mt-16">
       <p className="text-gray-600 mb-4">
-        Made with <span role="img" aria-label="heart">💙</span> by <a className="text-blue-500 hover:underline" href="https://github.com/balfajr">Balfajr</a>
+        Made with <span role="img" aria-label="heart"></span> by <a className="text-gray-500 hover:underline" href="https://github.com/balfajr">Balfajr</a>
       </p>
     </div>
   )
--- src/partials/ProjectCard.js
+++ src/partials/ProjectCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
-import GitHubButton from "react-github-btn";
+// import GitHubButton from "react-github-btn";
 
 const ProjectCard = ({ project: { title, description, tags, link } }) => {
   return (
     <div className="group w-full sm:w-1/2 m-4 mx-auto p-6 rounded-xl border-2 border-gray-300">
       <a href={link}>
-        <h1 className="text-xl text-center font-bold hover:underline transition duration-500 ease-in-out">
+        <h1 className="text-xl text-center font-bold underline hover:no-underline transition duration-500 ease-in-out">
           {title}{" "}
           <FaExternalLinkAlt className="inline align-baseline" />
         </h1>
